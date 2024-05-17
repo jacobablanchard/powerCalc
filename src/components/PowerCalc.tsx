73f48d8461632b4f89be876cef895b7ca1b40ca3
Jacob Blanchard <jacobablanchard@gmail.com>
2024-05-17 15:26:11 -0500

    Add a footer

--- src/components/PowerCalc.tsx
+++ src/components/PowerCalc.tsx
@@ -5,6 +5,7 @@ import type { PowerPlan } from "./PowerPlanInput/types";
 import PowerPlanDisplayAndInput from "./PowerPlanInput";
 import CostPlot from "./CostPlot";
 import CostTable from "./CostTable";
+import Footer from "./Footer";
 
 interface IPowerCalcProps {}
 
@@ -52,6 +53,7 @@ const PowerCalc: React.FunctionComponent<IPowerCalcProps> = (props) => {
         powerUsage={powerUsage}
         startMonth={month}
       />
+      <Footer />
     </div>
   );
 };
